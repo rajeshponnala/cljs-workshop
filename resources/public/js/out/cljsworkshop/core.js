12485c065c6a923bb7c84e83a01547f205f42a59
Your Full Name <Your EMail Id>
2015-10-08 01:53:52 +0530

    add dom events

--- resources/public/js/out/cljsworkshop/core.js
+++ resources/public/js/out/cljsworkshop/core.js
@@ -1,6 +1,8 @@
 // Compiled by ClojureScript 0.0-2843 {}
 goog.provide('cljsworkshop.core');
 goog.require('cljs.core');
+goog.require('goog.events');
+goog.require('goog.dom');
 cljsworkshop.core.set_html_BANG_ = (function set_html_BANG_(el,content){
 return el.innerHTML = content;
 });
@@ -9,6 +11,20 @@ var content = "Hello World from ClojureScript";
 var element = (document.getElementsByTagName("main")[(0)]);
 return cljsworkshop.core.set_html_BANG_.call(null,element,content);
 });
-cljsworkshop.core.main.call(null);
+cljsworkshop.core.main1 = (function main1(){
+var counter = cljs.core.atom.call(null,(0));
+var button = goog.dom.getElement("button");
+var display = goog.dom.getElement("clicksnumber");
+display.innerHTML = cljs.core.deref.call(null,counter);
+
+return goog.events.listen(button,"click",((function (counter,button,display){
+return (function (event){
+cljs.core.swap_BANG_.call(null,counter,cljs.core.inc);
+
+return display.innerHTML = cljs.core.deref.call(null,counter);
+});})(counter,button,display))
+);
+});
+cljsworkshop.core.main1.call(null);
 
 //# sourceMappingURL=core.js.map
