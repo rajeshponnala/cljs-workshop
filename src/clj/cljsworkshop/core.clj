8c87ccc70d6dbffa1ac91bf175b524330eee2efb
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-10-07 16:42:24 +0530

    start cljoure script

---
+++ src/clj/cljsworkshop/core.clj
@@ -0,0 +1,22 @@
+(ns cljsworkshop.core
+  (:require [qbits.jet.server :refer [run-jetty]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.response :refer [render]]
+            [clojure.java.io :as io]))
+
+(defn home
+  ""
+  [req]
+  (render (io/resource "index.html") req))
+
+(defroutes app
+  (GET "/" [] home)
+  (route/resources "/static")
+  (route/not-found "<h1>Page not found</h1>"))
+
+(defn -main
+  [& args]
+  (let [app (wrap-defaults app site-defaults)]
+    (run-jetty {:ring-handler app :port 5050})))
