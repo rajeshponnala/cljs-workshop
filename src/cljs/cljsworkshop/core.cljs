295d9ea8c4b560e246e6196b8bc8a608b66989fc
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-10-07 17:47:06 +0530

    add dom events

--- src/cljs/cljsworkshop/core.cljs
+++ src/cljs/cljsworkshop/core.cljs
@@ -1,4 +1,6 @@
-(ns cljsworkshop.core)
+(ns cljsworkshop.core
+  (:require [goog.events :as events]
+            [goog.dom :as dom]))
 
 (defn set-html! [el content]
   (set! (.-innerHTML el) content))
@@ -9,4 +11,17 @@
         element (aget (js/document.getElementsByTagName "main") 0)]
     (set-html! element content)))
 
-(main)
+(defn main1
+  []
+  (let [counter (atom 0)
+        button (dom/getElement "button")
+        display (dom/getElement "clicksnumber")]
+
+    (set! (.-innerHTML display) @counter)
+    (events/listen button "click"
+                   (fn [event]
+                     (swap! counter inc)
+                     (set! (.-innerHTML display) @counter)))))
+
+;;(main)
+(main1)
